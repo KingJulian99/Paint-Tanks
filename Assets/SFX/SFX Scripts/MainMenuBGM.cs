27227cd20a6191ba8e822b5d948db10e592cf418
ReeceBank <66268949+ReeceBank@users.noreply.github.com>
2022-10-21 16:35:22 +0200

    added tonnes of UI icons. Mainmenu has music. restructured SFX folders

---
+++ Assets/SFX/SFX Scripts/MainMenuBGM.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuBGM : MonoBehaviour
+{
+    AudioSource mainMenuBGM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float bmgvolume;
+        if (PlayerPrefs.HasKey("bgmvolume"))
+        {
+            Debug.Log("The key bgmvolume exists");
+            bmgvolume = PlayerPrefs.GetFloat("bgmvolume");
+        }
+        else
+        {
+            Debug.Log("The key bgmvolume doesnt exist");
+            PlayerPrefs.SetFloat("bgmvolume", 1.0f);
+            bmgvolume = 1.0f;
+        }
+        Debug.Log("Playing audio source " + mainMenuBGM);
+
+        mainMenuBGM = GetComponent<AudioSource>();
+        mainMenuBGM.volume = bmgvolume;
+        mainMenuBGM.Play();
+
+    }
+
+    public void MuteBGM(){
+        mainMenuBGM.volume = 0.0f;
+    }
+}
